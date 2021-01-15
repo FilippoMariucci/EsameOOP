cc837b0536e5dd54bfafcf2e716e6744c72391c5
Mariuccifilippo <mariuccifilippo@outlook.it>
2021-01-15 16:11:07 +0100

    Package Repository

---
+++ src/main/java/Repository/MeteoRepository.java
@@ -0,0 +1,17 @@
+package Repository;
+
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MeteoRepository extends CrudRepository<SpazioVariabili,Long> {
+    @Query(value = "SELECT * FROM Meteo WHERE CITY_ID = :CityId AND EPOCH >= :start AND EPOCH <= :stop", nativeQuery = true)
+    List<SpazioVariabili> trovaValori(@Param("CityId") String CityId,
+                                      @Param("start") long start,
+                                      @Param("stop") long stop);
+}
