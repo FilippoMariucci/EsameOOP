e3621a2950475a572dd3ffb9c342e3832654f825
Mariuccifilippo <mariuccifilippo@outlook.it>
2021-01-15 16:20:38 +0100

    Package Controller

---
+++ src/main/java/Controller/Richiesta.java
@@ -0,0 +1,146 @@
+package Controller;
+
+import Eccezioni.EccezioniStatistiche;
+import Eccezioni.TimeTravelerExeption;
+import Model.SpazioVariabili;
+import Repository.MeteoRepository;
+import Utilities.MeteoUtilities;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.security.InvalidParameterException;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+abstract class Richiesta extends MeteoUtilities {
+    private static final Logger logger = LoggerFactory.getLogger(Richiesta.class);
+
+    /**
+     * database su cui effettuare le varie ricerche
+     */
+
+    protected MeteoRepository meteoRepository;
+    /**
+     * Filtro ricevuto dalla richiesta dell'api
+     */
+    protected JSONObject filter;
+
+    /**
+     * JSONObject utilizzato per costruire la nostra risposta
+     */
+
+    protected JSONObject answer;
+
+    /**
+     * tipologia di grandezza richiesta
+     */
+    protected String type;
+
+    /**
+     * JSONArray che conterrà tutte le città richieste
+     */
+
+    protected JSONArray cities;
+
+    /**
+     * JSONObject che conterrà il periodo di ricerca richiesto dall'utente
+     */
+
+    private JSONObject period;
+
+    /**
+     * istante di partenza per la ricerca (UNIX)
+     */
+    protected Long start;
+
+    /**
+     * istante di fine per la ricerca (UNIX)
+     */
+
+    protected Long stop;
+
+    /**
+     * array contenente  la possibile grandezza
+     */
+    protected String[] types = { "temperature"};
+
+    /**
+     * Costruttore con paremetri MeteoRepository, Filtro
+     */
+
+    public Richiesta(JSONObject  filter, MeteoRepository meteoRepository){
+        this.meteoRepository=meteoRepository;
+        this.filter=filter;
+        this.answer=new JSONObject();
+    }
+
+
+    /**
+     *Metodo astratto che si occupa di costruire la risposta. il metodo sarà svolto nella sua sottoclasse
+     */
+
+    public abstract JSONObject getResult() throws EccezioniStatistiche;
+
+    /**
+     *Metodo che si occupa di effettuare un primo parse dalla richesta
+     */
+
+    protected boolean fisrtParseRequest(){
+        try {
+            if((this.cities = (JSONArray) this.filter.get("cities")) == null)
+                return false;
+            if((this.period= (JSONObject) this.filter.get("period")) != null) {
+                date2epoch();
+            } else
+                return false;
+
+            if ((this.type=(String) this.filter.get("type"))==null)
+                return false;
+
+            return true;
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return false;
+        }
+    }
+
+
+    private void date2epoch() throws TimeTravelerExeption, InvalidParameterException {
+
+        String from = (String) this.period.get("from");
+        String to = (String) this.period.get("to");
+
+        if ((from != null) && (to != null)) { // il parsing è riuscito
+
+            DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                    .withZone(ZoneOffset.systemDefault());
+
+            if ((Instant.from(fmt.parse(from)).toEpochMilli() / 1000) < (Instant.from(fmt.parse(to)).toEpochMilli()
+                    / 1000)) {
+                this.start = Instant.from(fmt.parse(from)).toEpochMilli() / 1000;
+                this.stop = Instant.from(fmt.parse(to)).toEpochMilli() / 1000;
+
+            } else {
+                throw new TimeTravelerExeption("Intervallo non Valido");
+            }
+
+        } else {
+            throw new InvalidParameterException("Parametri di ricerca non valide");
+        }
+    }
+
+    protected Double getValue(SpazioVariabili spazioVariabili, String type){
+        switch (type){
+            case "temperature":
+                return spazioVariabili.getTemp();
+
+            default:
+                return -999.0;
+
+        }
+
+    }
+}
---
+++ src/main/java/Controller/RichiestaStatistiche.java
@@ -0,0 +1,87 @@
+package Controller;
+
+import Eccezioni.EccezioniStatistiche;
+import Model.SpazioVariabili;
+import Repository.MeteoRepository;
+import Utilities.StatisticCalculator;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+@SuppressWarnings("unchecked")
+public class RichiestaStatistiche extends Richiesta{
+    private static final Logger logger= LoggerFactory.getLogger(RichiestaStatistiche.class);
+
+    /**
+     * costruttore con parametri
+     * @Param meteoRepository è l'archivio dati su cui effettuare le ricerche
+     * @Param filtro è un filtro di ricerca ricevuto nelle api
+     */
+
+    public RichiestaStatistiche(JSONObject filter, MeteoRepository meteoRepository){
+        super(filter,meteoRepository);
+    }
+
+    /**
+     * Effettuo l'overriding del metodo della superclasse
+     * Ritorna la rispota in formato JSON all'utente
+     */
+    @Override
+    public JSONObject getResult()throws EccezioniStatistiche{
+        if (fisrtParseRequest()){
+            JSONArray result= new JSONArray();
+            this.answer.put("code",0);
+            this.answer.put("info","");
+
+            for (Object city :cities){
+                String CityId=(String) city;
+                logger.info(CityId);// giusto per controllare
+
+                if (this.type.equals("all")){
+                    for (String type:this.types){
+                        result.add(calcolaStatistiche(CityId,type));
+                    }
+                }else {
+                    result.add(calcolaStatistiche(CityId,this.type));
+                }
+
+            }
+            this.answer.put("result",result);
+        }else {
+            this.answer=Generarisposta(3,"File not ok",0L);
+        }
+        return this.answer;
+    }
+
+
+    /**
+     * Metodo che si occupa di costruire la risposta in formato JSON
+     * da ritornare al chiamante
+     * Effettua la query al repository
+     * Istanzia un oggetto di StatisticCalculator performando i calcoli richiesti
+     * sui dati estratti
+     */
+
+    private JSONObject calcolaStatistiche(String CityId,String type)throws EccezioniStatistiche {
+        List<SpazioVariabili> spazioVariabilis = meteoRepository.trovaValori(CityId,this.start,this.stop);
+        JSONObject risultatiPerCityId=new JSONObject();
+        risultatiPerCityId.put("CityId",CityId);
+        risultatiPerCityId.put("type",type);
+
+        StatisticCalculator statisticCalculator=new StatisticCalculator();
+        for (SpazioVariabili spazioVariabili:spazioVariabilis){
+            statisticCalculator.addSpazioVaribili(getValue(spazioVariabili,type));
+        }
+        JSONObject data =new JSONObject();
+        data.put("max",statisticCalculator.getMax());
+        data.put("min",statisticCalculator.getMin());
+        data.put("media",statisticCalculator.getMedia());
+        data.put("varianza", statisticCalculator.getVarianza());
+        risultatiPerCityId.put("data",data);
+        return risultatiPerCityId;
+
+    }
+}
