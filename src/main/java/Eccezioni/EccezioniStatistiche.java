3e0b533e27e6947723619e577e733870a74d6009
Mariuccifilippo <mariuccifilippo@outlook.it>
2021-01-15 16:15:47 +0100

    Package Eccezioni

---
+++ src/main/java/Eccezioni/EccezioniStatistiche.java
@@ -0,0 +1,13 @@
+package Eccezioni;
+
+public class EccezioniStatistiche extends Exception {
+    public static final long serialVersion=3L;
+
+    public EccezioniStatistiche(){
+        super();
+    }
+
+    public EccezioniStatistiche(String messaggio){
+        super(messaggio);
+    }
+}
