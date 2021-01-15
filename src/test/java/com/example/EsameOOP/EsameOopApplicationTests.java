bec99fc0b0488d7cb579dd4bceb62a1e230a452a
Mariuccifilippo <mariuccifilippo@outlook.it>
2021-01-15 15:57:45 +0100

    Initial Commit

---
+++ src/test/java/com/example/EsameOOP/EsameOopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EsameOOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsameOopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
